25d14428060a6440db0222c8b787dd0c5d55e707
muhammad zakria <zakriamuhammad3637@gmail.com>
2021-09-14 13:09:35 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './App.css';
+import Header from "./Header";
+function App() {
+  return (
+    <div className="app">
+      <h1>Helllo Clever Programmer Lets
+        Build the Amzon Store :rock </h1>
+      <Header />
+    </div>)
+
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCWy69OaqL267N-GCnNa52dEGdQVneBggA",
+    authDomain: "challenge-807ec.firebaseapp.com",
+    projectId: "challenge-807ec",
+    storageBucket: "challenge-807ec.appspot.com",
+    messagingSenderId: "561624000698",
+    appId: "1:561624000698:web:ac3d3317153715a3d2fa83",
+    measurementId: "G-E6E34HS2HE"
+  };
